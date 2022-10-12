4f159a82ed795c1f922dbb8b20464b39ec89aab3
Mücahit YILMAZ <mucahit_yilmaz@outlook.com>
2022-10-12 15:15:09 +0300

    First commit.

---
+++ src/main/java/com/mucahit/pdfexample/Viewer.java
@@ -0,0 +1,38 @@
+package com.mucahit.pdfexample;
+
+import java.io.ByteArrayInputStream;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.icepdf.ri.common.ComponentKeyBinding;
+import org.icepdf.ri.common.SwingController;
+import org.icepdf.ri.common.SwingViewBuilder;
+
+/**
+ *
+ * @author mucahit.yilmaz
+ */
+public class Viewer {
+
+    public static void view(byte[] bytes) {
+        SwingController controller = new SwingController();
+//        var bundle = ResourceBundle.getBundle(PropertiesManager.DEFAULT_MESSAGE_BUNDLE);
+//        var pm = new PropertiesManager(System.getProperties(), bundle);
+
+        SwingViewBuilder factory = new SwingViewBuilder(controller);
+
+        JPanel viewerComponentPanel = factory.buildViewerPanel();
+
+        ComponentKeyBinding.install(controller, viewerComponentPanel);
+
+        controller.getDocumentViewController().setAnnotationCallback(
+                new org.icepdf.ri.common.MyAnnotationCallback(
+                        controller.getDocumentViewController()));
+
+        JFrame window = new JFrame("Using the Viewer Component");
+        window.getContentPane().add(viewerComponentPanel);
+        window.pack();
+        window.setVisible(true);
+
+        controller.openDocument(new ByteArrayInputStream(bytes), "", null);
+    }
+}
